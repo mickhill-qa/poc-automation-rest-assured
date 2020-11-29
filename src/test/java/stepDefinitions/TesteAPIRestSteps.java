4df7d2a79757d63d36663886c00e727971ec1dbd
Mick Hill - MacBook <mickhill@hotmail.com.br>
2020-11-28 21:11:49 -0300

    Acrecentando teste de contrato ao cenario de teste ja existente

--- src/test/java/stepDefinitions/TesteAPIRestSteps.java
+++ src/test/java/stepDefinitions/TesteAPIRestSteps.java
@@ -1,14 +1,16 @@
 package stepDefinitions;
 
+import java.io.FileNotFoundException;
+import baseClass.BaseSteps;
+import endpointModels.LoginEndpoint;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.junit.Assert;
-
-import baseClass.BaseSteps;
-import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.E;
 import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Quando;
-import endpointModels.LoginEndpoint;
 import io.restassured.response.Response;
 
 public class TesteAPIRestSteps extends BaseSteps {
@@ -35,11 +37,9 @@ public class TesteAPIRestSteps extends BaseSteps {
 	 * FluxoBase
 	 **/
 	@Quando("^o usuario tenta logar com dados validos$")
-	public void o_usuario_tenta_logar_com_dados_validos() {
+	public void o_usuario_tenta_logar_com_dados_validos() throws FileNotFoundException {
 		
-		JSONObject bodyJson = new JSONObject();
-		bodyJson.put("email", "eve.holt@reqres.in");
-		bodyJson.put("password", "cityslicka");
+		JSONObject bodyJson = BaseSteps.loadJsonRequestBody("login-dados-validos.json");
 		
 		endpointLogin.sendRequest(bodyJson.toString());
 		BaseSteps.attachJsonInReport(bodyJson.toString());
@@ -61,5 +61,16 @@ public class TesteAPIRestSteps extends BaseSteps {
 		
 		Assert.assertNotNull(tokenExist);
 		BaseSteps.attachJsonInReport(response.body().asString());
+	}
+
+	@E("^a API retorna o JSON de acordo com o contrato$")
+	public void a_API_retorna_o_JSON_de_acordo_com_o_contrato() throws FileNotFoundException {
+		
+		Response response 		= endpointLogin.getResponse();
+		JSONObject jsonResponse = new JSONObject( response.body().asString() );
+		
+		JSONObject jsonSchema 	= BaseSteps.loadJsonApiContracts("login-schema.json");
+	    Schema schema 			= SchemaLoader.load(jsonSchema);
+	    schema.validate(jsonResponse);
 	}
 }
--- src/test/resources/features/TesteAPIRest.feature
+++ src/test/resources/features/TesteAPIRest.feature
@@ -10,3 +10,4 @@ Funcionalidade: Teste de API Rest
 		Quando o usuario tenta logar com dados validos
 		Entao a API retorna status code 200
 		E a API retorna o token de acesso
+		E a API retorna o JSON de acordo com o contrato
